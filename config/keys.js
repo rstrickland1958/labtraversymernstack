cca4a7d3e9f1498df6acf74bdc6172e31895e82f
Robert Strickland <robertstrickland1958@gmail.com>
2018-07-04 11:07:03 -0400

    chore(mernstack) Added mongodb connection.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://rstrickland1958:yfmamJ58@ds121871.mlab.com:21871/traversy_mern_shopping'
+}
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.json());
+
+// DB Configuration
+const mongoDBURI = require('./config/keys').mongoURI;
